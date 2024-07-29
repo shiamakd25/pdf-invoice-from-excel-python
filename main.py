215e1534cf63a5b26e1dcfcb3994df665b3d3790
shiamakd25 <shiamakdas@outlook.com>
2024-07-29 13:58:07 -0400

    Create a basic pdf for each invoice containing invoice number

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import glob
 import openpyxl
+from fpdf import FPDF
+from pathlib import Path
 
 files = glob.glob("invoices/*.xlsx")
-print(files)
 
 for file in files:
     df = pd.read_excel(file, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    file_name = Path(file).stem
+    invoice_num = file_name.split("-")[0]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice Number {invoice_num}")
+    pdf.output(f"pdfs/{file_name}.pdf")
