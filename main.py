de3532b8e612ae9f485c16bfdfe9e631e228bd31
shiamakd25 <shiamakdas@outlook.com>
2024-07-29 14:49:21 -0400

    Added total price and footer

--- main.py
+++ main.py
@@ -37,7 +37,25 @@ for file in files:
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
         pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
-        pdf.cell(w=20, h=8, txt=str(row["total_price"]), border=1, ln=1)
+        pdf.cell(w=30, h=8, txt=f"${str(row['price_per_unit'])}", border=1)
+        pdf.cell(w=20, h=8, txt=f"${str(row['total_price'])}", border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=70, h=8, txt="")
+    pdf.cell(w=30, h=8, txt="")
+    pdf.cell(w=40, h=8, txt="")
+    pdf.cell(w=30, h=8, txt="")
+    pdf.cell(w=20, h=8, txt=f"${str(total_sum)}", border=1, ln=1)
+
+    pdf.ln(10)
+
+    pdf.set_font(family="Times", size=12)
+    pdf.cell(w=30, h=8, txt=f"The total price is ${total_sum}", ln=1)
+
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.cell(w=30, h=8, txt="Shiamak Das")
 
     pdf.output(f"pdfs/{file_name}.pdf")
